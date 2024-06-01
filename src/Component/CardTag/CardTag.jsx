eb43d6941ff8032b596bf523495a760a92bc54bf
Bharatchandran <bharatchandrankr@gmail.com>
2024-06-01 16:49:19 +0530

    Make CardTag component DRY

--- src/Component/CardTag/CardTag.jsx
+++ src/Component/CardTag/CardTag.jsx
@@ -1,41 +1,49 @@
-import React from 'react'
-import adsIcon from "../../assets/advertisement.png"
+import React, { useEffect, useState } from 'react'
 const CardTag = ({tag}) => {
-    const getTag = (tag) => {
+    const [selectTag, letSelectTag] = useState({
+        color: "",
+        icon: ""
+    })
 
+    useEffect( () => {
         switch (tag) {
             case 'Recommended':
-                return <div className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-1 rounded-full dark:bg-green-900 dark:text-white flex items-center justify-between">{tag}   
-                    <span className="material-symbols-outlined ">
-                        reviews
-                    </span>
-                </div>;
+                letSelectTag({
+                    color: "green",
+                    icon: "reviews"
+                })
+                break
             case 'Trending':
-                return <div className="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-1 rounded-full dark:bg-red-900 dark:text-white flex items-center justify-between">
-                    {tag}  
-                    <span className="material-symbols-outlined ">
-                    trending_up
-                    </span> 
-                </div>
+                letSelectTag({
+                    color: "red",
+                    icon: "trending_up"
+                })
+                break
             case 'Ads':
-                return <div className="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-white flex items-center justify-between">
-                    {tag}
-                    <span className="material-symbols-outlined">
-                        sell
-                    </span>
-                </div>;
+                letSelectTag({
+                    color: "yellow",
+                    icon: "sell"
+                })
+                break
             default:
-                return <span class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                    {tag}
-                </span>;
+                letSelectTag({
+                    color: "blue",
+                    icon: ""
+                })
+                
           }
-    }
-
+        
+    },[tag])
+    
+    
   return (
     <div>
-      {
-        getTag(tag)
-      }
+      <div className={`bg-${selectTag.color}-100 text-${selectTag.color}-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-${selectTag.color}-900 dark:text-white flex items-center justify-between`}>
+                     {tag}
+                     <span className="material-symbols-outlined text-lg ml-1">
+                         {selectTag.icon}
+                     </span>
+                 </div>
     </div>
   )
 }
