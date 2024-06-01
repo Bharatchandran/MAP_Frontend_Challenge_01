d3a86b7cecf4a70f1e7464cc4efe3057fe48c07d
Bharatchandran <bharatchandrankr@gmail.com>
2024-06-01 08:48:54 +0530

    set up initial configuration for React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Component/Card/Card"
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-6xl">Hello World</h1>
+     <Card />
+    </>
+  )
+}
+
+export default App
